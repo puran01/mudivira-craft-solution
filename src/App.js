82bc719f307cde3bbcf7dbef4b7aea8480063385
PURANDHAR_pv <104832373+Vittalan@users.noreply.github.com>
2024-01-18 00:29:02 +0530

    home page skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Homeimage from "./assets/Home.jpg"
+import LOGO from "./logo.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex-column">
+      <div className="d-flex w-100 align-items-center justify-content-between">
+        <div className="">
+          <img src={LOGO} style={{ width: 50 }} alt="Logo" />
+        </div>
+        <div className="text-center">
+          <h1>Mudivira Design Studio</h1>
+        </div>
+        <div className=""></div>
+      </div>
+      <div className='d-flex flex-row' style={{height:"93vh"}}>
+        <div className=" d-flex align-items-center  justify-content-center p-5 " >
+          <h1 style={{fontWeight:700, fontSize:70,  textAlign:"left"}} >Your Dreams are designed here!</h1>
+        </div>
+        <div className="d-flex justify-content-center align-items-center pr-5" >
+          <img src={Homeimage} style={{ width: "80%", height: "80%", borderRadius: 150 }} alt="Home" />
+        </div>
+      </div>
     </div>
   );
 }
